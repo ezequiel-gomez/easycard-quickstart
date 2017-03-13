dac46615222477a699bb1e9be8a1f576812d76db
ezequiel-gomez <eze.angel.gomez@gmail.com>
2017-03-12 23:44:55 -0300

    core refactor

--- client/client.js
+++ client/client.js
@@ -1,6 +1,76 @@
 var socket = io();
 
+socket.on('connect', function () {
+  socket.emit('newplayer', newPlayer());
+});
+
+function newPlayer () {
+  var playerName = prompt("Cuál es tu nombre?");
+  if (playerName !== null) {
+    return {name: playerName};
+  }
+}
+
 socket.on('updatechat', onMessage);
+socket.on('enablechatinput', enableChatInput);
+socket.on('enablebettinginput', enableBettingInput);
+
+function enableChatInput () {
+  var form = document.createElement('form'),
+      input = document.createElement('INPUT'),
+      button = document.createElement('BUTTON'),
+      container = document.getElementsByClassName('input_controls')[0];
+
+  input.setAttribute('autocomplete', 'off');
+  input.id = 'chat-input';
+  button.className = 'enviar';
+  button.innerHTML = 'Enviar';
+
+  form.appendChild(input);
+  form.appendChild(button);
+
+  container.appendChild(form);
+
+  form.addEventListener('submit', function(e) {
+    var input = document.getElementById('chat-input');
+    var message = input.value;
+    if (message.trim() != '') {
+      input.value = '';
+      socket.emit('sendchat', message);
+    }
+    e.preventDefault();
+  });
+}
+
+function enableBettingInput () {
+  var form = document.createElement('form'),
+      input = document.createElement('INPUT'),
+      button = document.createElement('BUTTON'),
+      container = document.getElementsByClassName('input_controls')[0];
+
+  input.setAttribute('autocomplete', 'off');
+  input.setAttribute('placeholder', 'Monto');
+  input.setAttribute('type', 'number');
+  input.setAttribute('maxlength', '7');
+  input.id = 'betting-input';
+  button.className = 'enviar';
+  button.innerHTML = 'Apostar';
+
+  form.appendChild(input);
+  form.appendChild(button);
+
+  container.appendChild(form);
+
+  form.addEventListener('submit', function(e) {
+    var input = document.getElementById('betting-input');
+    var message = input.value;
+    if (message.trim() != '') {
+      input.value = '';
+      socket.emit('sendbet', message);
+    }
+    e.preventDefault();
+  });
+}
 
 function onMessage (username, data) {
   var list = document.getElementById('chat-text');
--- server.js
+++ server.js
@@ -1,26 +1,21 @@
 'use strict'
 
-
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-require('./lib/easycard');
-
-io.on('connection', onConnection);
-
 app.use(express.static(__dirname + '/client'));
-
 var port = Number(process.env.PORT || 8080);
-
 server.listen(port, () => console.log('Ready to work!.'));
 
+require('./lib/easycard');
+
 var myGame = easyCard.initNewGame();
+
+io.on('connection', onConnection);
 
 function onConnection(socket) {
   myGame.addPlayer(socket);
